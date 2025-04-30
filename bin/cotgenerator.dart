8a538cfbf21a78dcd5ac1ad31cc453b4117fc71b
Mitch Winkle <mitch@radcommsoft.com>
2025-04-30 14:01:25 -0400

    add ability to build CoT as XML

--- bin/cotgenerator.dart
+++ bin/cotgenerator.dart
@@ -2,9 +2,10 @@ import 'package:args/args.dart';
 import 'generator.dart';
 
 const String version = '0.0.1';
+bool doXML = false; // def. to use pBuf generator
 
 ArgParser buildParser() {
-  return ArgParser()
+  return ArgParser(allowTrailingOptions: false)
     ..addFlag(
       'help',
       abbr: 'h',
@@ -15,13 +16,19 @@ ArgParser buildParser() {
       'debug',
       abbr: 'd',
       negatable: false,
-      help: 'Show program trace.',
+      help: 'Show program trace.'
     )
     ..addFlag(
       'version',
       abbr: 'v',
       negatable: false,
-      help: 'Print the version string.',
+      help: 'Print the version string.'
+    )
+    ..addFlag(
+      'xml',
+      abbr: 'x',
+      negatable: false,
+      help: 'Use the XML generator.'
     );
 }
 
@@ -37,6 +44,10 @@ void main(List<String> arguments) {
     bool debug = false;
 
     // Process the parsed arguments.
+    if(results.flag('xml')) {
+      // use the XML generator instead of the pBuf generator
+      doXML = true;
+    }
     if (results.flag('help')) {
       printUsage(argParser);
       return;
@@ -54,25 +65,37 @@ void main(List<String> arguments) {
     if (debug) {
       print('[DEBUG] All arguments: ${results.arguments}');
     }
-    switch(results.arguments.length) {
+    
+    //switch(results.arguments.length) {
+    switch(results.rest.length) {
+      case 6:
+      print('6 Arguments: Make ${results.rest[0]} CoTs, Base Name: ${results.rest[1]} Every ${results.rest[2]} seconds');
+      final g = Generator(xml: doXML, numPLIs: int.parse(results.rest[0]), baseName: results.rest[1], timeout: int.parse(results.rest[2]), lat : double.parse(results.rest[3]), lon : double.parse(results.rest[4]), endpoint: results.rest[5]);
+      g.launchCOTs();
+      break;
+    case 5:
+      print('5 Arguments: Make ${results.rest[0]} CoTs, Base Name: ${results.rest[1]} Every ${results.rest[2]} seconds');
+      final g = Generator(xml: doXML, numPLIs: int.parse(results.rest[0]), baseName: results.rest[1], timeout: int.parse(results.rest[2]), lat : double.parse(results.rest[3]), lon : double.parse(results.rest[4]));
+      g.launchCOTs();
+      break;
     case 3:
-      print('3 Arguments: Make ${results.arguments[0]} CoTs, Base Name: ${results.arguments[1]} Every ${results.arguments[2]} seconds');
-      final g = Generator(numPLIs: int.parse(results.arguments[0]), baseName: results.arguments[1], timeout: int.parse(results.arguments[2]));
+      print('3 Arguments: Make ${results.rest[0]} CoTs, Base Name: ${results.rest[1]} Every ${results.rest[2]} seconds');
+      final g = Generator(xml: doXML, numPLIs: int.parse(results.rest[0]), baseName: results.rest[1], timeout: int.parse(results.rest[2]));
       g.launchCOTs();
       break;
     case 2:
-      print('2 Arguments: Make ${results.arguments[0]} CoTs, Base Name: ${results.arguments[1]} Every 30 seconds');
-      final g = Generator(numPLIs: int.parse(results.arguments[0]), baseName: results.arguments[1]);
+      print('2 Arguments: Make ${results.rest[0]} CoTs, Base Name: ${results.rest[1]} Every 30 seconds');
+      final g = Generator(xml: doXML, numPLIs: int.parse(results.rest[0]), baseName: results.rest[1]);
       g.launchCOTs();
       break;
     case 1:
-      print('1 Argument: Make ${int.parse(results.arguments[0])} CoTs, Base Name: COTGEN Every 30 seconds');
-      final g = Generator(numPLIs: int.parse(results.arguments[0]), baseName: 'COTGEN');
+      print('1 Argument: Make ${int.parse(results.rest[0])} CoTs, Base Name: COTGEN Every 30 seconds');
+      final g = Generator(xml: doXML, numPLIs: int.parse(results.rest[0]), baseName: 'COTGEN');
       g.launchCOTs();
       break;
     default:
       print('0 Arguments: Make 12 CoTs, Base Name: COTGEN Every 30 seconds');
-      final g = Generator(numPLIs: 12, baseName: 'COTGEN');
+      final g = Generator(xml: doXML, numPLIs: 12, baseName: 'COTGEN');
       g.launchCOTs();
     }
   } 
--- bin/generator.dart
+++ bin/generator.dart
@@ -5,6 +5,7 @@ import 'dart:collection';
 import 'package:fixnum/fixnum.dart';
 import 'dart:io';
 import 'dart:math';
+import 'package:xml/xml.dart';
 // import 'dart:typed_data';
 
 import 'takproto/takmessage.pb.dart';
@@ -14,16 +15,20 @@ import 'takproto/detail.pb.dart';
 class Generator{
 List<int> delim = [191,1,191];
 int _numPLIs = 12;
+double _lat = 0.0;
+double _lon = 0.0;
+bool _useXML = false;
 String _baseName = 'COTGEN';
 int _timeout = 30;
-String endpoint = "192.168.4.129:4242:tcp";
-final int maxPins = 13; // max number of PLI's to generate + 1 for loop
+String _endpoint = "192.168.4.129:4242:tcp";
+// final int maxPins = 13; // max number of PLI's to generate + 1 for loop
 Map<String, List<int> > cots = HashMap();
 List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", "Purple", "Cyan"];
     
-  Generator({int numPLIs = 12, String baseName = 'COTGEN', int timeout = 30}) 
-  : _numPLIs = numPLIs, _baseName = baseName, _timeout = timeout
+  Generator({bool xml = false, int numPLIs = 12, String baseName = 'COTGEN', int timeout = 30, double lat = 36.95, double lon = -77.67, String endpoint = "192.168.4.129:4242:tcp"}) 
+  : _useXML = xml, _numPLIs = numPLIs, _baseName = baseName, _timeout = timeout, _lat = lat, _lon = lon, _endpoint = endpoint
   {
+    //print('Use XML? $_useXML');
     Timer.periodic(Duration(milliseconds: _timeout * 1000), (timer) {launchCOTs();});    
   }
 
@@ -41,8 +46,13 @@ List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", 
     stdout.write("$ts Launch CoTs\r");
     loadCOTs();
     cots.forEach((String key, value) {      
-      // print("Delim:${delim + value}");
-      writeDatagram(delim + value, "239.2.3.1", 6969);
+      //print("CoT:${delim + value}");
+      if(_useXML) {
+        writeDatagram(value, "239.2.3.1", 6969);
+      }
+      else {
+        writeDatagram(delim + value, "239.2.3.1", 6969);
+      }
       sleep(Duration(milliseconds: 100));
     });
   }
@@ -54,13 +64,79 @@ List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", 
         final group = groups[groupIdx];
         final name = "$_baseName$j";
         final tsdt = "${DateTime.now().toUtc().millisecondsSinceEpoch}";
-        List<int> blob = makeCOT(name, tsdt, group);
-        cots[name] = blob;
+        if(_useXML) {
+          cots[name] = makeXML(name, tsdt, group, seq: j).codeUnits;
+        }
+        else {
+          // List<int> blob = makeCOT(name, tsdt, group);
+          cots[name] = makeCOT(name, tsdt, group, seq: j);
+        }
+        // cots[name] = blob;
     }
   }
   
   void on_Send_Now_triggered(){
     launchCOTs();
+  }
+
+  String makeXmlTs(int ts, int plusMs) {
+    var valms = ts + plusMs;
+    var out = DateTime.fromMillisecondsSinceEpoch(valms).toIso8601String();
+    return out;
+  }
+
+  String makeXML(String uid, String dateTime, String group, {int seq = 1}) {
+    /*
+<event version="2.0" 
+uid="mattermost-user-" 
+type="a-f-G-U-C" 
+time="2025-04-29T11:44:23.64Z" 
+start="2025-04-29T11:44:23.64Z" 
+stale="2025-04-29T11:47:53.64Z" 
+how="h-g" 
+access="Undefined">
+
+    <detail><contact callsign="ROVER" /><__gotenna messageForCallsign="ROVER" messageForUUID="mattermost-user-" /></detail>
+
+    <point lat="36.9515336" lon="-77.6736624" hae="49.7799835205078" ce="9999999" le="9999999" />
+    */
+    // make an xml string out of the CoT data
+    //String out = '<?xml version="1.0" encoding="utf-8" ?>';
+    // append the rest of the tags here and
+    var ms = int.parse(dateTime);
+    var builder = XmlBuilder();
+    builder.processing('xml', 'version="1.0"');
+    builder.element('event', nest: () {
+      builder.attribute('version', '2.0');
+      builder.attribute('uid', uid);
+      builder.attribute('type', 'a-f-G-U-C');
+      builder.attribute('time', makeXmlTs(ms, 0));
+      builder.attribute('start', makeXmlTs(ms, 0));
+      builder.attribute('stale', makeXmlTs(ms, 120000));
+      builder.attribute('how', 'm-g');
+      builder.attribute('access', 'Undefined');
+      builder.element('point', nest:  (){
+        builder.attribute('lat', '36.95');
+        builder.attribute('lon', '-77.67');
+        builder.attribute('hae', '49.78');
+        builder.attribute('ce', '9999999');
+        builder.attribute('le', '9999999');
+      });
+      builder.element('detail', nest: () {
+        builder.element('contact', nest: (){
+          builder.attribute('callsign', uid);          
+        });
+        builder.element('__gotenna', nest: () {
+          builder.attribute('messageForCallsign', uid);
+          builder.attribute('messageForUUID', uid);
+        });
+      }); // end <detail>
+    });
+    
+    // return the results
+    var out = builder.buildDocument().toXmlString(pretty: false); 
+    print("$_endpoint: ${out.substring(out.indexOf('?>') + 2)}");
+    return out.substring(out.indexOf('?>') + 2);
   }
 
   List<int> makeCOT(String uid, String dateTime, String group, {int seq = 1}){
@@ -86,16 +162,16 @@ List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", 
     evt.le = 9999999;
     evt.ce = 9999999;
     if(seq.isOdd) {
-        evt.lat = (36.95 - randyLat);
+        evt.lat = (_lat - randyLat);
     }
     else {
-        evt.lat = (36.95 + randyLat - 0.022);
+        evt.lat = (_lat + randyLat - 0.022);
     }
     if(seq.isOdd) {
-        evt.lon = (-77.67 - randy + 0.017);
+        evt.lon = (_lon - randy + 0.017);
     }
     else {
-        evt.lon = (-77.67 + randy);
+        evt.lon = (_lon + randy);
     }
     evt.how = "h-e";
     evt.startTime = Int64(int.parse(dateTime));
@@ -105,7 +181,7 @@ List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", 
 
     var detail = Detail(); //evt.detail.createEmptyInstance();
     //detail.xmlDetail = "<contact callsign=\"$uid\" endpoint=\"$endpoint\"/><uid Droid=\"$uid\"/><__group name=\"$group\" role=\"$role\"/>";
-    detail.setField(1, '<contact callsign="$uid" endpoint="$endpoint"/><uid Droid="$uid"/><__group name="$group" role="$role"/>');
+    detail.setField(1, '<contact callsign="$uid" endpoint="$_endpoint"/><uid Droid="$uid"/><__group name="$group" role="$role"/>');
     evt.setField(15, detail);
     t.setField(2, evt);
     //  print("\n$t");
