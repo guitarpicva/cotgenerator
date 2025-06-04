847299f2a2604f6cacaeecb5c9c24671ed13c4b2
Mitch Winkle <mitch@radcommsoft.com>
2025-06-04 14:50:30 -0400

    added xml builder

--- bin/generator.dart
+++ bin/generator.dart
@@ -1,29 +1,27 @@
 import 'dart:async';
 import 'dart:collection';
-// import 'dart:ffi';
-// import 'dart:ffi';
 import 'package:fixnum/fixnum.dart';
 import 'dart:io';
 import 'dart:math';
 import 'package:xml/xml.dart';
-// import 'dart:typed_data';
 
 import 'takproto/takmessage.pb.dart';
 import 'takproto/cotevent.pb.dart';
 import 'takproto/detail.pb.dart';
 
 class Generator{
-List<int> delim = [191,1,191];
-int _numPLIs = 12;
+final List<int> delim = [191,1,191];
+final List<int> xmlheader = '<?xml version="1.0"?>'.codeUnits;
+int _numPLIs =  6;
+bool _useXML = false;
 double _lat = 0.0;
 double _lon = 0.0;
-bool _useXML = false;
 String _baseName = 'COTGEN';
 int _timeout = 30;
 String _endpoint = "192.168.4.129:4242:tcp";
 // final int maxPins = 13; // max number of PLI's to generate + 1 for loop
 Map<String, List<int> > cots = HashMap();
-List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", "Purple", "Cyan"];
+List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", "Purple", "Dark_Blue", "Blue", "Cyan", "Teal", "Green", "Dark_Green", "Brown"];
     
   Generator({bool xml = false, int numPLIs = 12, String baseName = 'COTGEN', int timeout = 30, double lat = 36.95, double lon = -77.67, String endpoint = "192.168.4.129:4242:tcp"}) 
   : _useXML = xml, _numPLIs = numPLIs, _baseName = baseName, _timeout = timeout, _lat = lat, _lon = lon, _endpoint = endpoint
@@ -42,50 +40,48 @@ List<String> groups = ["White", "Yellow", "Orange", "Magenta", "Red", "Maroon", 
   }
   
   void launchCOTs(){
-    final ts = "[${DateTime.now().toUtc().toIso8601String()}]";
-    stdout.write("$ts Launch CoTs\r");
+    //final ts = "[${DateTime.now().toUtc().toIso8601String()}]";
+    //stdout.write("$ts Launch CoTs\n");
     loadCOTs();
-    cots.forEach((String key, value) {      
-      //print("CoT:${delim + value}");
+    cots.forEach((String key, value) {                  
       if(_useXML) {
-        writeDatagram(value, "239.2.3.1", 6969);
+        print("CoT:${String.fromCharCodes(xmlheader + value)}");
+        writeDatagram(xmlheader + value, "127.0.0.1", 19791);
       }
       else {
-        writeDatagram(delim + value, "239.2.3.1", 6969);
+        print("CoT:${String.fromCharCodes(delim + value)}");
+        writeDatagram(delim + value, "127.0.0.1", 19791);
       }
       sleep(Duration(milliseconds: 100));
     });
   }
     
   void loadCOTs() {
-    for (int j = 1; j < (_numPLIs + 1); ++j) {
-        int groupIdx = j % 8; // 0 - 7
+    final int numplis = _numPLIs + 1;
+    for (int j = 1; j < numplis; ++j) {
+        int groupIdx = j % 14; // 0 - 13
         //print("cots:$cots");
         final group = groups[groupIdx];
         final name = "$_baseName$j";
         final tsdt = "${DateTime.now().toUtc().millisecondsSinceEpoch}";
         if(_useXML) {
-          cots[name] = makeXML(name, tsdt, group, seq: j).codeUnits;
+          cots[name] = makeXMLCoT(name, tsdt, group, seq: j).codeUnits;
         }
         else {
           // List<int> blob = makeCOT(name, tsdt, group);
-          cots[name] = makeCOT(name, tsdt, group, seq: j);
+          cots[name] = makePbCoT(name, tsdt, group, seq: j);
         }
         // cots[name] = blob;
     }
   }
-  
-  void on_Send_Now_triggered(){
-    launchCOTs();
-  }
-
+ 
   String makeXmlTs(int ts, int plusMs) {
     var valms = ts + plusMs;
     var out = DateTime.fromMillisecondsSinceEpoch(valms).toIso8601String();
-    return out;
+    return "${out}Z";
   }
 
-  String makeXML(String uid, String dateTime, String group, {int seq = 1}) {
+  String makeXMLCoT(String uid, String dateTime, String group, {int seq = 1}) {
     /*
 <event version="2.0" 
 uid="mattermost-user-" 
@@ -100,11 +96,33 @@ access="Undefined">
 
     <point lat="36.9515336" lon="-77.6736624" hae="49.7799835205078" ce="9999999" le="9999999" />
     */
+    //print('Make and XML CoT: seq: $seq');    
     // make an xml string out of the CoT data
     //String out = '<?xml version="1.0" encoding="utf-8" ?>';
     // append the rest of the tags here and
-    var ms = int.parse(dateTime);
+    final String faketakv = '<takv ';
+    final int ms = int.parse(dateTime);
     var builder = XmlBuilder();
+    final double randy = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
+    // randy *= 0.003;
+    final double randyLat = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
+    // randyLat *= 0.003;
+    final double randyHAE = Random.secure().nextInt(99) * 1.0; // int 0 -- 99
+    var lat = 0.0;
+    var lon = 0.0;
+    if(seq.isOdd) {
+        lat = (_lat - randyLat);
+    }
+    else {
+        lat = (_lat + randyLat - 0.022);
+    }
+    if(seq.isOdd) {
+        lon = (_lon - randy + 0.017);
+    }
+    else {
+        lon = (_lon + randy);
+    }
+    final String role = "Ground Troop";
     builder.processing('xml', 'version="1.0"');
     builder.element('event', nest: () {
       builder.attribute('version', '2.0');
@@ -116,15 +134,29 @@ access="Undefined">
       builder.attribute('how', 'm-g');
       builder.attribute('access', 'Undefined');
       builder.element('point', nest:  (){
-        builder.attribute('lat', '36.95');
-        builder.attribute('lon', '-77.67');
-        builder.attribute('hae', '49.78');
+        builder.attribute('lat', lat);
+        builder.attribute('lon', lon);
+        builder.attribute('hae', randyHAE);
         builder.attribute('ce', '9999999');
         builder.attribute('le', '9999999');
       });
       builder.element('detail', nest: () {
+        // builder.element('uid', nest:() {
+        //   builder.attribute('Droid', uid);
+        // });
+        builder.element('takv', nest: (){
+          builder.attribute('device', 'x');
+          builder.attribute('platform', 'ATAK');
+          builder.attribute('os', 'x');
+          builder.attribute('version', 'x');
+        });
         builder.element('contact', nest: (){
-          builder.attribute('callsign', uid);          
+          builder.attribute('callsign', uid);       
+          builder.attribute('endpoint', '192.168.4.38:4242:tcp');   
+        });
+        builder.element('__group', nest: () {
+          builder.attribute('role', role);
+          builder.attribute('name', group);
         });
         builder.element('__gotenna', nest: () {
           builder.attribute('messageForCallsign', uid);
@@ -139,24 +171,23 @@ access="Undefined">
     return out.substring(out.indexOf('?>') + 2);
   }
 
-  List<int> makeCOT(String uid, String dateTime, String group, {int seq = 1}){
+  List<int> makePbCoT(String uid, String dateTime, String group, {int seq = 1}){
     List<int> out = [];
-
 // use some boilerplate to build a COT with the given uid and time stamp    
     TakMessage t = TakMessage.create(); // a Protobuf Message
     // build the message part by part here
     // t.takControl().setMinProtoVersion(1);
     // t.takControl().setMaxProtoVersion(1);
     // t.takControl().setContactUid(uid);
-    double randy = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
+    final double randy = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
     // randy *= 0.003;
-    double randyLat = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
+    final double randyLat = Random.secure().nextInt(9) * 0.003; // int 0 -- 9
     // randyLat *= 0.003;
-    double randyHAE = Random.secure().nextInt(99) * 1.0; // int 0 -- 99
+    final double randyHAE = Random.secure().nextInt(99) * 1.0; // int 0 -- 99
     final String role = "Ground Troop";
     // print("randy:$randy");
     var evt = CotEvent(); //cotEvent.createEmptyInstance();    
-    evt.type = "a-f-G-u-c";   
+    evt.type = "a-f-G-U-C";   
     evt.uid = uid;
     evt.hae = randyHAE;
     evt.le = 9999999;
